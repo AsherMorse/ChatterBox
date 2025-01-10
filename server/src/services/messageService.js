3dda93db65a99c3f084c51379e8a734b2e4f5eac
Asher Morse <ashermorse@icloud.com>
2025-01-10 14:25:55 -0800

    Remove Socket.IO as we are using Supabase Realtime instead

--- server/src/services/messageService.js
+++ server/src/services/messageService.js
@@ -6,9 +6,7 @@ const supabase = createClient(
 );
 
 class MessageService {
-    constructor(io) {
-        this.io = io;
-        this.setupSocketHandlers = this.setupSocketHandlers.bind(this);
+    constructor() {
         this.setupRealtimeSubscription();
     }
 
@@ -22,16 +20,8 @@ class MessageService {
                     table: 'messages'
                 },
                 (payload) => {
-                    // Handle message changes
-                    if (payload.eventType === 'INSERT') {
-                        const message = payload.new;
-                        // Broadcast to appropriate channel
-                        if (message.channel_id) {
-                            this.io.to(`channel:${message.channel_id}`).emit('new_message', message);
-                        } else if (message.dm_id) {
-                            this.io.to(`dm:${message.dm_id}`).emit('new_message', message);
-                        }
-                    }
+                    // Handle message changes through Supabase Realtime
+                    console.log('Message change:', payload);
                 }
             )
             .subscribe();
@@ -63,79 +53,6 @@ class MessageService {
         }
 
         return data;
-    }
-
-    async getChannelMessages(channelId, limit = 50) {
-        const { data, error } = await supabase
-            .from('messages')
-            .select(`
-                *,
-                sender:sender_id(id, username, avatar_url)
-            `)
-            .eq('channel_id', channelId)
-            .order('created_at', { ascending: false })
-            .limit(limit);
-
-        if (error) {
-            console.error('Error fetching messages:', error);
-            throw error;
-        }
-
-        return data.reverse();
-    }
-
-    setupSocketHandlers(socket) {
-        // Join a channel
-        socket.on('join_channel', (channelId) => {
-            socket.join(`channel:${channelId}`);
-            console.log(`${socket.user.username} joined channel ${channelId}`);
-        });
-
-        // Leave a channel
-        socket.on('leave_channel', (channelId) => {
-            socket.leave(`channel:${channelId}`);
-            console.log(`${socket.user.username} left channel ${channelId}`);
-        });
-
-        // Send a message
-        socket.on('send_message', async (message) => {
-            try {
-                const savedMessage = await this.saveMessage({
-                    ...message,
-                    sender_id: socket.user.id
-                });
-
-                // The message will be broadcasted through Supabase Realtime
-                socket.emit('message_sent', savedMessage);
-            } catch (error) {
-                socket.emit('message_error', {
-                    message: 'Error sending message',
-                    error: error.message
-                });
-            }
-        });
-
-        // Start typing
-        socket.on('typing_start', (channelId) => {
-            socket.to(`channel:${channelId}`).emit('user_typing', {
-                user: {
-                    id: socket.user.id,
-                    username: socket.user.username
-                },
-                channelId
-            });
-        });
-
-        // Stop typing
-        socket.on('typing_stop', (channelId) => {
-            socket.to(`channel:${channelId}`).emit('user_stopped_typing', {
-                user: {
-                    id: socket.user.id,
-                    username: socket.user.username
-                },
-                channelId
-            });
-        });
     }
 }
 
